074a344d3c2cd52b412445f6882b215072976d3c
Jarad Solomon <jaradsolomon@gmail.com>
2021-05-08 00:41:40 -0500

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,53 @@
 // Code your solutions in this file
+
+/*
+for (let age = 30; age < 40; age++) {
+    console.log(`I'm ${age} years old. Happy birthday to me!`);
+    debugger;
+}
+*/
+/*
+const gifts = ["teddy bear", "drone", "doll"];
+
+function wrapGifts(gifts) {
+  for (let i = 0; i < gifts.length; i++) {
+    console.log(`Wrapped ${gifts[i]} and added a bow!`);
+   
+  }
+
+  return gifts;
+}
+
+wrapGifts(gifts); 
+*/
+
+/*
+const event = `surprise`;
+const name = []
+*/
+
+
+const newArr = [];
+
+function writeCards(names, event){ 
+    for (let i = 0; i < names.length; i++){
+
+        newArr.push(`Thank you, ${names[i]}, for the wonderful ${event} gift!`);
+        debugger;
+
+    }
+    return newArr;
+}
+
+
+
+function countDown(integer){
+    
+ while (integer >= 0) {
+
+    console.log(integer);
+    integer--;
+ }
+}
+
+//writeCards(['Lisa', 'Kaitlin', 'Jan' ], 'surprise' )
